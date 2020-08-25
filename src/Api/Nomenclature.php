ce73bf49f123054188a52c539e5bea7f3a9cf5e6
Max Kashkin <4kotrpa@gmail.com>
2020-08-25 13:36:24 +0800

    reworked nomenclature api to trait

---
+++ src/Api/Nomenclature.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace KMA\IikoApi\Api;
+
+use KMA\IikoApi\Entity\Nomenclature as Entity;
+use KMA\IikoApi\Exceptions\IikoApiException;
+
+use KMA\IikoApi\Iiko;
+use KMA\IikoApi\Traits\Http;
+use JsonMapper;
+
+/**
+ * Trait Nomenclature
+ * @package KMA\IikoApi\Api
+ *
+ * @mixin Http
+ * @mixin Iiko
+ */
+trait Nomenclature
+{
+    /**
+     * @param string $orgId iiko organization id
+     * @return Entity KMA\IikoApi\Entity\Nomenclature
+     * @throws \JsonMapper_Exception
+     * @throws IikoApiException
+     */
+    public function nomenclature(string $orgId): Entity
+    {
+        $endpoint = '/nomenclature/' . $orgId;
+
+        $params = [
+            'access_token' => $this->token(),
+        ];
+
+        $response = $this->get($endpoint, $params);
+        // $json = json_decode($response->getBody(), true);
+        return (new JsonMapper())->mapArray(
+            $response->getDecodedBody(), new Entity()
+        );
+    }
+}
